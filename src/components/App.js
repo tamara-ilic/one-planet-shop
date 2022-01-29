ced59f4b88932123e7b8ac598727439ca62816d9
tamara <tamara@ilicmcfarlane.io>
2022-01-29 17:32:27 +0000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,89 @@
+import '../styles/App.css';
+import { createClient } from 'contentful';
+import { useState, useEffect } from 'react';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+
+// Main configuration
+const config = {
+  spaceID: '6xrk35zptngz',
+  deliveryToken: 'hxA-YncKJ-haBXbtQF9l1Ffv-suunBrm1iW1lbmGPJI'
+};
+
+// Create client
+const client = createClient({
+  space: config.spaceID,
+  accessToken: config.deliveryToken
+});
+
+export const getProducts = () => {
+  // Retrieve all entries of a space
+  return client.getEntries({
+    content_type: 'product',
+    order: 'sys.createdAt'
+  });
+};
+
+export default function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((res) => {
+      const items = res.items;
+
+      const productListings = items.map((item, index) => {
+        return {
+          ...item,
+          file: item.fields.productMedia[0].fields.file,
+          title: item.fields.title,
+          img: item.fields.productMedia[0].fields.file.url
+        };
+      });
+      setProducts(productListings);
+    });
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>Sustainable products that don't cost the Earth.</h1>
+      {products.map((p) => (
+        <>
+          <h4>{p.fields.title}</h4>
+          <p>€ {p.fields.price}</p>
+          <img className='product-image' src={p.img} alt={p.file.title} />
+          <>
+            {documentToReactComponents(p.fields.description, {
+              _renderNode: {
+                [BLOCKS.PARAGRAPH]: (node, children) => {
+                  return <div className='product-description'>{children}</div>;
+                },
+                [BLOCKS.HEADING_3]: (node, children) => {
+                  return <h3>{children}</h3>;
+                },
+                [BLOCKS.UL_LIST]: (node, children) => {
+                  return <ul>{children}</ul>;
+                }
+              },
+              get renderNode() {
+                return this._renderNode;
+              },
+              set renderNode(value) {
+                this._renderNode = value;
+              },
+
+              renderMark: {
+                [MARKS.CODE]: (text) => <code className='red'>{text}</code>
+              },
+              renderText: (text) => {
+                return text
+                  .split('\n')
+                  .map((i) => [i, <br />])
+                  .flat();
+              }
+            })}
+          </>
+        </>
+      ))}
+    </div>
+  );
+}
