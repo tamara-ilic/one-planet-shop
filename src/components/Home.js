2b5e976677ddaee002db276f295670479e9f89b3
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-02-17 21:15:36 +0000

    add shop now button to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-  export default function Home() {
-    return (
-      <main>
-        <h1 className='tagline'>Sustainable products that don't cost the Earth.</h1>
-        <p>Hey, so you want to shop responsibly and on a budget? We offer products that are both sustainable and affordable - it shouldn’t be a trade off.</p>
-        <img className='shop-symbols' src={require('../assets/one-planet-cruelty-free-conscious-plastic-free.png')} alt='cruelty-free, conscious, plastic-free symbols'></img>
-      </main>
+import { NavLink } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className='tagline'>Sustainable products that don't cost the Earth.</h1>
+      <p>Hey, so you want to shop responsibly and on a budget? We offer products that are both sustainable and affordable - it shouldn’t be a trade off.</p>
+      <img className='shop-symbols' src={require('../assets/one-planet-cruelty-free-conscious-plastic-free.png')} alt='cruelty-free, conscious, plastic-free symbols'></img>
+      <NavLink to='/shop'>
+        <button className='button-shop'>SHOP NOW</button>
+      </NavLink>
+    </main>
     )
   }
