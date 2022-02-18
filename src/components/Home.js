5d4bdf661134c49455d5319ca7f77e2fa12af44b
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-02-18 23:10:51 +0000

    add styles for large breakpoint WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,11 @@ export default function Home() {
       <h1 className='tagline'>Sustainable products that don't cost the Earth.</h1>
       <p>Hey, so you want to shop responsibly and on a budget? We offer products that are both sustainable and affordable - it shouldn’t be a trade off.</p>
       <img className='shop-symbols' src={require('../assets/one-planet-cruelty-free-conscious-plastic-free.png')} alt='cruelty-free, conscious, plastic-free symbols'></img>
-      <NavLink to='/shop'>
-        <button className='button-shop'>SHOP NOW</button>
-      </NavLink>
+      <div>
+        <NavLink to='/shop'>
+          <button className='button-shop'>SHOP NOW</button>
+        </NavLink>
+      </div>
     </main>
     )
   }
