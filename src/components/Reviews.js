3b39d07df7f8412ce0d833cbe50c61d735bd7af9
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-02-18 18:17:18 +0000

    render new reviews WIP

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,10 +3,18 @@ import { ShopContext } from '../contexts/shopContext'
 
 export default function Reviews() {
     const { reviews } = useContext(ShopContext)
+    const [reviewHeadline, setReviewHeadline] = useState('')
     const [newReview, setNewReview] = useState('')
 
     const handleReviewSubmit = (e) => {
         e.preventDefault()
+    }
+
+    const handleReviewHeadline = (e) => {
+        if (e.keyCode === 13) {
+            console.log(e.keyCode)
+            setReviewHeadline(e.target.value)
+        } else return
     }
 
     const handleNewReview = (e) => {
@@ -19,20 +27,22 @@ export default function Reviews() {
     return (
         <main>
             {/* Below is a hack. New reviews should be added to a database along with existing ones. */}
-            {newReview &&
+            {reviewHeadline && newReview &&
                 <div className='new-reviews'>
-                    {newReview}
+                    <h4>{reviewHeadline}</h4>
+                    <p>{newReview}</p>
                 </div>
             }
             {reviews.map((r) => (
                 <div key={reviews.id}>
                     <h4>{r.fields.headline}</h4>
                     <p>{r.fields.review}</p>
+                    {/* {r.fields.rating} */}
                 </div>
             ))}
             <h4>Leave a review</h4>
             <form onSubmit={handleReviewSubmit}>
-                <input className='review-headline' placeholder='Headline'></input>
+                <input onKeyUp={handleReviewHeadline} className='review-headline' placeholder='Headline'></input>
                 <textarea onKeyUp={handleNewReview} placeholder="Tried one of our products? We'd love to hear from you!"></textarea>
                 <input type='submit'></input>
             </form>
